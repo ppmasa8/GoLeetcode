eb7b6e62e8d05c44f2824be1bb0e1f32c32aa1b9
ppmasa8 <kiyokiyopip56@gmail.com>
2021-09-27 00:31:21 +0900

    Add linked list cycle 2

---
+++ LinkedListCycle2.go
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func detectCycle(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return nil
+	}
+	fast, slow := head, head
+
+	for fast != nil {
+		if fast.Next != nil {
+			fast = fast.Next.Next
+		} else {
+			return nil
+		}
+		slow = slow.Next
+
+		if fast == slow {
+			break
+		}
+	}
+
+	if fast == nil {
+		return nil
+	}
+
+	slow = head
+	for fast != slow {
+		slow = slow.Next
+		fast = fast.Next
+	}
+	return slow
+}
