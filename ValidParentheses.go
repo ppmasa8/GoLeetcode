b5f92cb29e2bcdca8d5d7a60ff8f4c31df363b45
ppmasa8 <kiyokiyopip56@gmail.com>
2021-09-29 00:11:18 +0900

    Add

---
+++ ValidParentheses.go
@@ -0,0 +1,20 @@
+func isValid(s string) bool {
+	stack := make([]rune, 0)
+	m := map[rune]rune {
+		')': '(',
+		']': '[',
+		'}': '{',
+	}
+	for _, v := range s {
+		switch v {
+		case '(', '{', '[' :
+			stack = append(stack, v)
+		case ')', '}', ']' :
+			if len(stack) == 0 || stack[len(stack)-1] != m[v] {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+	return len(stack) == 0
+}
