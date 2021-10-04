ef6ef7cf62c90b55a962064dc94a22cdb6bf6f2c
ppmasa8 <kiyokiyopip56@gmail.com>
2021-10-04 12:35:03 +0900

    Add

---
+++ Unrelational.go
@@ -0,0 +1,10 @@
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int, len(nums))
+	for i, v := range nums {
+		if idx, ok := m[target - v]; ok {
+			return []int{idx, i}
+		}
+		m[v] = i
+	}
+	return []int{0, 0}
+}
