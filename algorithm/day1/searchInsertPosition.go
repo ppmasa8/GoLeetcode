4d8398f5dc4d74d302573ddfcbbcde906c403121
ppmasa8 <kiyokiyopip56@gmail.com>
2021-10-16 00:04:10 +0900

    Add

---
+++ algorithm/day1/searchInsertPosition.go
@@ -0,0 +1,18 @@
+package main
+
+func searchInsert(nums []int, target int) int {
+	left := 0
+	right := len(nums) -1
+	for left <= right {
+		mid := (left+right)/2
+		if nums[mid] > target {
+			right = mid - 1
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else {
+			return mid
+		}
+	}
+
+	return left
+}
