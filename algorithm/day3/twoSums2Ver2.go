3442fb7073c1794d0f1c6be3a609dfdbd19a4ac6
ppmasa8 <kiyokiyopip56@gmail.com>
2021-10-17 15:16:36 +0900

    Add

---
+++ algorithm/day3/twoSums2Ver2.go
@@ -0,0 +1,15 @@
+package main
+
+func twoSum(numbers []int, target int) []int {
+	i, j := 0, len(numbers)-1
+	for {
+		if numbers[i] + numbers[j] < target {
+			i++
+		} else if numbers[i] + numbers[j] > target {
+			j--
+		} else {
+			return []int{i+1, j+1}
+		}
+	}
+	return []int{}
+}
