044ef9170ff1d9f011676d582260a2b471f75e15
ppmasa8 <kiyokiyopip56@gmail.com>
2022-01-10 00:26:39 +0900

    Add

---
+++ aNumberAfterADoubleReversal.go
@@ -0,0 +1,11 @@
+func isSameAfterReversals(num int) bool {
+    if num == 0 {
+        return true
+    }
+    
+    if num % 10 == 0 {
+        return false
+    } else {
+        return true
+    }
+}
