303fedd2b1e8da3553eb9e590e9699657bc93a50
ppmasa8 <kiyokiyopip56@gmail.com>
2022-06-22 19:27:19 +0900

    add

---
+++ Incomplete/matrixDiagonalSum.go
@@ -0,0 +1,6 @@
+func diagonalSum(mat [][]int) int {
+    n, ans := len(mat), 0
+    for i := 0; i < n; i++ { ans += mat[i][i] + mat[i][n - i - 1] }
+    if n % 2 == 1 { return ans - mat[n / 2][n / 2] }
+    return ans
+}
